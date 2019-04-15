55073a01bc6b660dc57a8290eb9bcc28e1901fae
Adam Treister <adam.treister@gladstone.ucsf.edu>
2019-04-15 11:40:17 -0700

    xx

--- src/main/java/org/cytoscape/enhancer/EnhancerController.java
+++ src/main/java/org/cytoscape/enhancer/EnhancerController.java
@@ -1,34 +1,15 @@
 package org.cytoscape.enhancer;
 
 import java.awt.Component;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-//import static org.cytoscape.ding.DVisualLexicon.NODE_CUSTOMGRAPHICS_1;
 
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.application.events.SetCurrentNetworkEvent;
 import org.cytoscape.application.events.SetCurrentNetworkListener;
 import org.cytoscape.application.swing.events.CytoPanelComponentSelectedEvent;
 import org.cytoscape.application.swing.events.CytoPanelComponentSelectedListener;
-//import org.cytoscape.ding.customgraphics.NullCustomGraphics;
-//import org.cytoscape.ding.impl.visualproperty.CustomGraphicsVisualProperty;
-import org.cytoscape.model.CyColumn;
 import org.cytoscape.model.CyNetwork;
-import org.cytoscape.model.CyNode;
-import org.cytoscape.model.CyRow;
-import org.cytoscape.model.CyTable;
 import org.cytoscape.service.util.CyServiceRegistrar;
 import org.cytoscape.view.model.CyNetworkView;
-import org.cytoscape.view.model.VisualLexicon;
-import org.cytoscape.view.model.VisualProperty;
-import org.cytoscape.view.presentation.RenderingEngineManager;
-import org.cytoscape.view.vizmap.VisualMappingFunction;
-import org.cytoscape.view.vizmap.VisualMappingFunctionFactory;
-import org.cytoscape.view.vizmap.VisualMappingManager;
-import org.cytoscape.view.vizmap.VisualStyle;
-import org.cytoscape.view.vizmap.VisualStyleFactory;
-import org.cytoscape.view.vizmap.mappings.PassthroughMapping;
 
 /*
  * EnhancerController
@@ -38,19 +19,16 @@ import org.cytoscape.view.vizmap.mappings.PassthroughMapping;
 
 public class EnhancerController implements CytoPanelComponentSelectedListener, SetCurrentNetworkListener {
 
-	private static final String ENHANCER_NAME = "egPie";
 	private CyServiceRegistrar registrar;
 	public EnhancerController(CyServiceRegistrar reg)
 	{
 		registrar = reg;
-		initialize();
 	}
 
 //----------------------------------------------------------
 	private CyApplicationManager cyApplicationManager;
 	private CyNetwork network;
 	private CyNetworkView networkView;
-	VisualMappingFunctionFactory factory;
 	private EnhancerPanel enhancerPanel;
 	public void setEnhancerPanel(EnhancerPanel p) { enhancerPanel = p; }
 	//----------------------------------------------------------
@@ -59,7 +37,7 @@ public class EnhancerController implements CytoPanelComponentSelectedListener, S
 		cyApplicationManager = registrar.getService(CyApplicationManager.class);
 		network = cyApplicationManager.getCurrentNetwork();
 		networkView = cyApplicationManager.getCurrentNetworkView();
-	}
+}
 	//-------------------------------------------------------------------
 	@Override
 	public void handleEvent(CytoPanelComponentSelectedEvent arg0) 
@@ -74,19 +52,15 @@ public class EnhancerController implements CytoPanelComponentSelectedListener, S
 		
 	public void scanNetwork() {	}
 	
-	
-	private void ensureCategoryCompatibility() {
-		if (network == null)  return;		// don't clear list until a conflict is found
-		List<String> extant = enhancerPanel.getColumnNames();
-		List<String> networkColumns = getColumnNames();
-		for (String col : extant)
-		{
-			CyColumn c =  network.getDefaultNodeTable().getColumn(col);
-			if (c == null)
-				enhancerPanel.clearCategories();
-		}
-	}
 
+	public void setCurrentNetView(CyNetworkView newView)
+	{
+		if (newView == null) return;		// use ""
+//		if (newView.getSUID() == currentNetworkView.getSUID()) return;
+		currentNetworkView = newView;
+		enhancerPanel.enableControls(currentNetworkView != null);
+		
+	}
 	//-------------------------------------------------------------------------------
 	private CyNetworkView currentNetworkView;
 	private boolean verbose = true;
@@ -102,113 +76,21 @@ public class EnhancerController implements CytoPanelComponentSelectedListener, S
 		networkView = cyApplicationManager.getCurrentNetworkView();
 
 	}
-	
-	List<String> getColumnNames()
-	{
-		List<String> strs = new ArrayList<String>();
-		CyNetwork net = cyApplicationManager.getCurrentNetwork();
-		if (net != null)
-		{
-			CyTable table = net.getDefaultNodeTable();
-			for (CyColumn col : table.getColumns())
-			{
-				if (null == col) 	continue;
-				if (!isNumeric(col)) continue;
-				if ("SUID".equals(col.getName())) continue;
-				strs.add(col.getName());
-			}
-		}
-		return strs;
-	}
-	
-	private boolean isNumeric(CyColumn col) {
-//		if (col == null) return false;
-		Class<?> c = col.getType();  //getClass();
-		String classs = c.getName();
-		if (classs.equals("java.lang.Integer")) return true;
-		if (classs.equals("java.lang.Long")) return true;
-		if (classs.equals("java.lang.Float")) return true;
-		if (classs.equals("java.lang.Double")) return true;
-		return false;
-	}
-
-	public void enhance(String extracted, List<String> colNames) {
+	public void enhance(String extract) {
+		System.out.println(extract);
 		
-		String spec = extracted;
-//		System.out.println(spec);
-
-		if (network == null) return ;
-		CyTable nodeTable = network.getDefaultNodeTable();
-		CyColumn col = nodeTable.getColumn(ENHANCER_NAME);
-		if (col == null)
-		{
-			nodeTable.createColumn(ENHANCER_NAME, String.class, false);
-			col = nodeTable.getColumn(ENHANCER_NAME);
-		}
-		for (CyRow row : nodeTable.getAllRows())
-			if (rowHasValues(nodeTable, row, colNames)) 
-				row.set(ENHANCER_NAME, extracted);
-
-		VisualStyle currentStyle = getStyle();
-
-		VisualMappingFunctionFactory passthroughFactory = 
-				registrar.getService(VisualMappingFunctionFactory.class, "(mapping.type=passthrough)");
-		VisualLexicon lex = registrar.getService(RenderingEngineManager.class).getDefaultVisualLexicon();
-
-// Set up the pass-through mapping
-
-		VisualProperty customGraphics = lex.lookup(CyNode.class, "NODE_CUSTOMGRAPHICS_1");
-		PassthroughMapping pMapping = 
-				(PassthroughMapping<String, ?>) passthroughFactory.createVisualMappingFunction(ENHANCER_NAME,   String.class, customGraphics);
-		currentStyle.addVisualMappingFunction(pMapping);
-		
-		networkView = cyApplicationManager.getCurrentNetworkView();
-		if (networkView != null)
-		{
-			currentStyle.apply(networkView);
-			networkView.updateView();
-		}
 	}
 
-	private boolean rowHasValues(CyTable nodeTable, CyRow row, List<String> colNames )
-	{
-		for (String column : colNames)
-		{
-			CyColumn col = nodeTable.getColumn(column);
-			Class<?> c = col.getType();
-			Object d = row.get(column, c);
-			if (d == null)	return false;
-		}
-		return true;
-	}
-	
-	private VisualStyle getStyle()
-	{
-		// Now we cruise thru the list of node, then edge attributes looking for mappings.  Each mapping may potentially be a legend entry.
-		VisualMappingManager manager = registrar.getService( VisualMappingManager.class);
-		VisualStyle style = manager.getCurrentVisualStyle();
-//		System.out.println("style: " + style.getTitle());
-		return style;
-
-	}
-	
 	//-------------------------------------------------------------------------------
 	@Override
 	public void handleEvent(SetCurrentNetworkEvent e) {
 
-		network = cyApplicationManager.getCurrentNetwork();
-		networkView = cyApplicationManager.getCurrentNetworkView();
-		setCurrentNetView(networkView);
-	}
-
-	public void setCurrentNetView(CyNetworkView newView)
-	{
-		if (newView == null) return;		// use ""
-//		if (newView.getSUID() == currentNetworkView.getSUID()) return;
-		currentNetworkView = newView;
-		currentNetworkView.getModel();
-		enhancerPanel.enableControls(currentNetworkView != null);
-		ensureCategoryCompatibility();
+		network = e.getNetwork();
+		if (cyApplicationManager != null)
+		{
+			networkView = cyApplicationManager.getCurrentNetworkView();
+//			scanNetwork();
+		}
 	}
 	
 }
--- src/main/java/org/cytoscape/enhancer/EnhancerPanel.java
+++ src/main/java/org/cytoscape/enhancer/EnhancerPanel.java
@@ -32,7 +32,6 @@ public class EnhancerPanel extends JPanel implements CytoPanelComponent {
 		controller.setEnhancerPanel(this);
 		buildUI();
 		setVisible(true);
-		addCategory();
 	}
 
 	//--------------------------------------------------------------------
@@ -43,78 +42,54 @@ public class EnhancerPanel extends JPanel implements CytoPanelComponent {
 	
 	public Component getComponent() 			{		return this;	}
 	public CytoPanelName getCytoPanelName() 	{		return CytoPanelName.WEST;	}
-	public String getTitle() 					{		return "Enhanced Graphics";	}
+	public String getTitle() 					{		return "Enhancer Panel";	}
 	public Icon getIcon() 						{		return null;	}
 
 	
 	JButton adder = new JButton("Add");
-	JButton newRing = new JButton("Add Ring");
 	JButton doIt = new JButton("Enhance");		
-	JButton link = new JButton("More...");		
 	JButton clearAll = new JButton("Clear");
 
 	List<ColumnMapPane> categories = new ArrayList<ColumnMapPane>();
-	int lineHeight = 32;
+	int lineHeight = 40;
 	Dimension dim = new Dimension(400, lineHeight);
-	Dimension dim4lines = new Dimension(400, 6*lineHeight);
-	Dimension dim12lines = new Dimension(400, 12*lineHeight);
-	Dimension introDim = new Dimension(400, 3*lineHeight);
+	Dimension dim4lines = new Dimension(400, 4*lineHeight);
 	Dimension numDimension = new Dimension(40, 30);
-	Dimension columnDimension = new Dimension(240, lineHeight);
-	Dimension adderDimension = new Dimension(36, 30);
+	Dimension columnDimension = new Dimension(140, lineHeight);
 	Dimension colorDimension = new Dimension(24, 24);
 	Dimension colorLabDimension = new Dimension(64, lineHeight);
 	Dimension rangeDimension = new Dimension(100, lineHeight);
-	
-	JComboBox<String> graphType;
-
 	
 JPanel makeIntro()
 {
 	JPanel intro = new JPanel();
 	intro.setLayout(new BoxLayout(intro, BoxLayout.PAGE_AXIS));
-	
-	JLabel label0 = new JLabel("A variety of graph types will be available.");
-	JLabel labelA = new JLabel("Only Pie Charts are supported at this point.");
-	LookAndFeelUtil.makeSmall(label0, labelA);
-   String types[] = { "Bar Chart", "Circos Chart", "Heat Strip Chart", "Line Chart", "Pie Chart", "Stripe Chart" }; 
-	graphType = new JComboBox<String>(types);
-	graphType.setSelectedItem("Pie Chart");
-//	graphType.setEnabled(false);
-	intro.add(line(label0));
-	intro.add(line(labelA));
-	intro.add(line(graphType));
-
-	JLabel label1 = new JLabel("Select the columns and colors to assign to the wedges of the pie.");
+	JLabel label1 = new JLabel("Select the columns and colors to assign to the pies.");
+	JLabel label2 = new JLabel("Range can be optionally set to get normalized colors.");
 	intro.add(line(label1));
-	intro.setBorder(BorderFactory.createEmptyBorder(6,6,0,6));
+	intro.add(line(label2));
+	intro.setBorder(BorderFactory.createEmptyBorder(6, 6, 6, 6));
 	LookAndFeelUtil.makeSmall(label1);
-	intro.add(Box.createVerticalGlue());
-	intro.setPreferredSize(introDim);
-	intro.setMaximumSize(introDim);
+	LookAndFeelUtil.makeSmall(label2);
 	return intro;
 
 }
 	JPanel makeHeader()
 	{
-		JButton adder = new JButton("+");
-		adder.addActionListener(addCategory);
 		JLabel lab1 = new JLabel("Column");
 		JLabel lab2 = new JLabel("Color");
-//		JLabel lab3 = new JLabel("Range");
-		LookAndFeelUtil.makeSmall(adder, lab1,lab2);		//, lab3
-		setSizes(adder, adderDimension);
+		JLabel lab3 = new JLabel("Range");
+		LookAndFeelUtil.makeSmall(lab1,lab2, lab3);
 		setSizes(lab1, columnDimension);
 		setSizes(lab2, colorLabDimension);
-//		setSizes(lab3, rangeDimension);
+		setSizes(lab3, rangeDimension);
 		JPanel line = new JPanel();
 		setSizes(line, dim);
 		line.setLayout(new BoxLayout(line, BoxLayout.LINE_AXIS));
-		line.add(adder);
-		line.add(Box.createHorizontalStrut(10));
+		line.add(Box.createHorizontalStrut(20));
 		line.add(lab1);
 		line.add(lab2);
-//		line.add(lab3);
+		line.add(lab3);
 		return line;
 		
 	}
@@ -126,135 +101,102 @@ JPanel makeIntro()
 		p.setPreferredSize(d);
 		
 	}
-	
-
-	//--------------------------------------------------------
 	class ColumnMapPane extends JPanel
 	{
 		JComboBox<String> column;
 		ColorMenuButton colorButton;
+		JTextField minVal;
+		JTextField maxVal;
 		String[] colNames = { "Name", "Age", "JurkatScore", "HEKScore" } ;
 			
-		private ColumnMapPane(List<String> columNameList)
+		private ColumnMapPane()
 		{
 			setSizes(this, dim);
-			column = new JComboBox<String>(columNameList.toArray(colNames)); 
-			column.setMaximumRowCount(40);
+			column = new JComboBox<String>(colNames); 	
 			column.setSize(columnDimension);
 			colorButton = new ColorMenuButton();
 			setSizes(colorButton,colorDimension); 
+			colorButton.setMinimumSize(colorDimension);
+			colorButton.setMaximumSize(colorDimension);
+			colorButton.setPreferredSize(colorDimension);
 			JPanel around = new JPanel();
 			around.add(colorButton);
+//			around.setBorder(BorderFactory.createDashedBorder(Color.blue));
+			//			colorButton.setSize(colorDimension);
+			minVal = new JTextField("0.0"); 				
+			setSizes(minVal,numDimension);
+			maxVal = new JTextField("1.0");				
+			maxVal.setSize(numDimension);
+			setSizes(maxVal,numDimension);
+//			colorButton.setBorder(BorderFactory.createLineBorder(Color.red));
 			setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
-			add(column);	add(Box.createHorizontalStrut(20));
+			add(column);		add(Box.createHorizontalStrut(20));
+//			JButton b = new JButton("Push");
+//			add(b);
 			add(around);	add(Box.createHorizontalStrut(20));
+			LookAndFeelUtil.makeSmall(minVal);
+			LookAndFeelUtil.makeSmall(maxVal);
+			JLabel lab1 = new JLabel("Min:");
+			JLabel lab2 = new JLabel("Max:");
+			LookAndFeelUtil.makeSmall(lab1);
+			LookAndFeelUtil.makeSmall(lab2);
+			add(lab1); add(minVal); add(Box.createHorizontalStrut(20));
+			add(lab2); add(maxVal);
+//			setBorder(BorderFactory.createLineBorder(Color.ORANGE));
 		}
 		
 		public String getColumn()	{ return "" + column.getSelectedItem(); }
 		public Color getCatColor()	{ return colorButton.getColor(); }
+		public double getMin()		{	return Double.parseDouble(minVal.getText());			}
+		public double getMax()		{	return Double.parseDouble(maxVal.getText());			}
 	}
-	//--------------------------------------------------------
-	ActionListener addCategory = new ActionListener() {
-		@Override public void actionPerformed(ActionEvent e) 
-		{
-			addCategory(); 
-			setVisible(false);
-			setVisible(true);
-		}
-	};
-	ActionListener addRing = new ActionListener() {
-		@Override public void actionPerformed(ActionEvent e) 
-		{
-			addCategory(); 
-			setVisible(false);
-			setVisible(true);
-		}
-	};
 	
 	private void addCategory()	
 	{ 	
-		List<String> names = controller.getColumnNames();
-		if (!names.isEmpty())
-		{
-			ColumnMapPane pane = new ColumnMapPane(names);
-			categories.add(pane);
-			categoryParentPanel.add(pane);
-		}
+		ColumnMapPane pane = new ColumnMapPane();
+		categories.add(pane);
+		categoryParentPanel.add(pane);
 	}
 	//--------------------------------------------------------------------
 	private void buildUI() {
 		
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		add(makeIntro());
-		add(makeCategoriesPanel());
-		add(Box.createRigidArea(new Dimension(20, 8)));
-
-		JLabel label2 = new JLabel("Range can be optionally set to get normalized colors.");
-		LookAndFeelUtil.makeSmall(label2);
-		add(line(label2));
-		add( makeRangePanel());
-		JLabel labelQ = new JLabel("Other arguments:  arcstart, labellist, labelsize, minimumslice, position");
-		JLabel labelR = new JLabel("scale, showlabels, size, valuelist, ybase");
-		LookAndFeelUtil.makeSmall(labelQ, labelR);
-		add(Box.createVerticalGlue());		
-		add(line(labelQ));
-		add(line(labelR));
-		add( makeButtonRow());
-		add(Box.createVerticalGlue());		
-	}
-
-	private Component makeCategoriesPanel() {
 		categoryParentPanel = new JPanel();
 //		optionsPanel.setAlignmentX(0f);
 		categoryParentPanel.setBorder(BorderFactory.createEtchedBorder());
-		categoryParentPanel.setMinimumSize(dim4lines);
-		categoryParentPanel.setPreferredSize(dim4lines);
-		categoryParentPanel.setMaximumSize(dim12lines);
 		categoryParentPanel.setLayout(new BoxLayout(categoryParentPanel, BoxLayout.PAGE_AXIS));
-		categoryParentPanel.add(Box.createRigidArea(new Dimension(10,3)));
+		categoryParentPanel.add(Box.createRigidArea(new Dimension(10,10)));
 		categoryParentPanel.add(makeHeader());
-		return categoryParentPanel;
-	}
+		add(categoryParentPanel);
+		categoryParentPanel.setMinimumSize(dim4lines);
+		add(Box.createRigidArea(new Dimension(20, 20)));
+		add(Box.createGlue());
 
-	private JPanel makeButtonRow() {
 		//a row of buttons and their actions
+		ActionListener addCategory = new ActionListener() {
+			@Override public void actionPerformed(ActionEvent e) 
+			{
+				addCategory(); 
+				setVisible(false);
+				setVisible(true);
+			}
+		};
 		adder.addActionListener(addCategory);
 
-//		newRing.addActionListener(addRing);
 
 		ActionListener clrAll = new ActionListener() {
-			@Override public void actionPerformed(ActionEvent e) { clearCategories();  }
+			@Override public void actionPerformed(ActionEvent e) { resetOptionsPanel();  }
 		};
 		clearAll.addActionListener(clrAll);
 
 		doIt.addActionListener(new ActionListener() {
-			@Override public void actionPerformed(ActionEvent e) { controller.enhance(extract(), getColumnNames());  }
+			@Override public void actionPerformed(ActionEvent e) { controller.enhance(extract());  }
 		});
-		return (line(doIt, clearAll, adder));		//, newRing
-				
-	}
-	JTextField minVal = new JTextField();
-	JTextField maxVal = new JTextField();
-	JLabel lab1 = new JLabel("Min:");
-	JLabel lab2 = new JLabel("Max:");
-
-	private JPanel makeRangePanel()
-	{
-		LookAndFeelUtil.makeSmall(minVal, maxVal, lab1,lab2 );
-		JPanel p = new JPanel();
-		p.setLayout(new BoxLayout(p, BoxLayout.LINE_AXIS));
-		p.add(lab1); p.add(minVal);
-		p.add(Box.createHorizontalStrut(20));
-		p.add(lab2); p.add(maxVal);
-		minVal.setPreferredSize(new Dimension(60, 30));
-		maxVal.setPreferredSize(new Dimension(60, 30));
-		minVal.setMaximumSize(new Dimension(60, 30));
-		maxVal.setMaximumSize(new Dimension(60, 30));
-		p.setMaximumSize(new Dimension(400, 30));
-		p.setPreferredSize(new Dimension(400, 30));
-		p.setBorder(BorderFactory.createEmptyBorder(6, 6, 6, 6));
-		return p;
-
+		add(line(doIt, clearAll, adder));
+		
+		addCategory();
+		add(Box.createVerticalGlue());		//-------
 	}
 
 	//--------------------------------------------------------------------
@@ -268,15 +210,12 @@ JPanel makeIntro()
 			category.setEnabled(on);
 	}
 	
-	public void clearCategories()
+	public void resetOptionsPanel()
 	{
 		categoryParentPanel.removeAll();
 		categoryParentPanel.setVisible(false);
 		categoryParentPanel.setVisible(true);
-		categories.clear();
-		categoryParentPanel.add(makeHeader());
-		addCategory();			// put one new category back
-			
+
 	}
 	//--------------------------------------------------------------------
 	public void setCurrentNetwork()
@@ -285,44 +224,22 @@ JPanel makeIntro()
 		enableControls(view != null);
 	}
 	//--------------------------------------------------------------------
-//http://www.cgl.ucsf.edu/cytoscape/utilities3/enhancedcg.shtml
-//  piechart: attributelist="Values" colorlist="contrasting" labellist="First,Second,Third,Fourth,Fifth"
-//    piechart: attributelist="a,b,c,d" colorlist="red,green,blue,yellow"
-
-	public List<String> getColumnNames()
-	{
-		List<String> strs = new ArrayList<String> ();
-		for (ColumnMapPane pane : categories)
-			strs.add(pane.getColumn());
-		return strs;
-		
-	}
 	public String extract()
 	{
-		StringBuilder builder = new StringBuilder("piechart: ");
 		// pull the data out of the GUI components
-		StringBuilder attributes = new StringBuilder("attributelist=\"");
-		StringBuilder colors = new StringBuilder("colorlist=\"");
+		StringBuilder builder = new StringBuilder();
 		for (ColumnMapPane pane : categories)
 		{
-			attributes.append(pane.getColumn()).append(",");
+			builder.append(pane.getColumn());
 			Color c = pane.getCatColor();
-			colors.append(Colors.toString(c)).append(",");
+			builder.append("\t").append(Colors.toString(c)).append("\t");
+			builder.append ("[").append(pane.getMin());
+			builder.append (",").append(pane.getMax()).append ("]\n");
 		}
-		String attr = changeLastCommaToQuoteSpace(attributes.toString());
-		String colrs = changeLastCommaToQuoteSpace(colors.toString());
-		builder.append(attr).append(colrs);
-		String min = minVal.getText().trim();
-		String max= maxVal.getText().trim();
-		if (!(min.isEmpty() || (max.isEmpty())))
-		{
-			StringBuilder ranges = new StringBuilder("range=\"");
-			ranges.append(minVal.getText()).append (",").append(maxVal.getText()).append ("\" ");
-			builder.append(ranges.toString());
-		}
-		builder.append("showlabels=\"false\" ");
+		builder.append ("\n");
 		return builder.toString();
 	}
+	
 	//--------------------------------------------------------------------
 	// util
 	
@@ -349,29 +266,7 @@ JPanel makeIntro()
 //		box.setBorder(BorderFactory.createLineBorder(Color.blue));
 		return box;
 	}
-	
-	private JPanel line(JComponent subA, JComponent subB, JComponent subC, JComponent subD)
-	{
-		JPanel box = new JPanel();
-		box.setLayout(new BoxLayout(box, BoxLayout.LINE_AXIS));
-		box.add(subA);
-		box.add(Box.createRigidArea(new Dimension(12,12)));
-		box.add(subB);
-		box.add(Box.createRigidArea(new Dimension(12,12)));
-		box.add(subC);
-		box.add(Box.createRigidArea(new Dimension(12,12)));
-		box.add(subD);
-		box.add(Box.createHorizontalGlue());
-//		box.setBorder(BorderFactory.createLineBorder(Color.blue));
-		return box;
-	}
 
-	
-	private String changeLastCommaToQuoteSpace(String attr) {
-		int idx = attr.lastIndexOf(",");
-		return idx < 0 ? "" : (attr.substring(0, idx) + "\" ");
-		
-	}
 
 }
 
